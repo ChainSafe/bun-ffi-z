00d6b8d287415022e0e2d58f9058597e1a02e0e0
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-05-12 11:20:00 +0700

    fix: do not export cli to default entry point

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from "./lib.ts";
 export * from "./build.ts";
 export * from "./prepublish.ts";
 export * from "./config.ts";
-export * from "./cli.ts";
+// do not export cli here or consumer project will call it accidentially
