cf12a4b15b6fb3227d40a59e83e6000000c064f2
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-05-15 15:20:48 +0700

    feat: add artifacts param to prepublish cli command

--- src/prepublish.ts
+++ src/prepublish.ts
@@ -1,7 +1,7 @@
 import {join} from "node:path";
+import {parseArgs, type ParseArgsOptionsConfig} from "node:util";
 import {getLibraryName, type Target, getTargetParts} from "./lib.ts";
 import { getConfigFromPkgJson, type Config, type Json } from "./config.ts";
-import { build } from "./build.ts";
 
 export async function writeTargetPackage(
   rootPkgJson: Json,
@@ -54,17 +54,17 @@ This is the ${target} target package for ${rootPkgJson.name}.
 export async function copyTargetLibrary(
   config: Config,
   target: Target,
+  artifactsDirPath: string,
 ): Promise<void> {
   const {platform} = getTargetParts(target);
   const libraryName = getLibraryName(config.name, platform);
-  const zigCwd = config.zigCwd;
 
-  const zigOutDir = join(zigCwd, "zig-out", "bin");
+  const targetArtifactDir = join(artifactsDirPath, target);
   const targetDir = join(process.cwd(), 'targetPackages', target);
 
   await Bun.write(
     join(targetDir, libraryName),
-    await Bun.file(join(zigOutDir, libraryName)).arrayBuffer(),
+    await Bun.file(join(targetArtifactDir, libraryName)).arrayBuffer(),
   );
 }
 
@@ -80,21 +80,38 @@ export async function updateOptionalDependencies(
   return pkgJson;
 }
 
+const prepublishOptions = {
+  "artifacts": {
+    type: "string",
+  },
+} satisfies ParseArgsOptionsConfig;
+
 export async function prepublish(): Promise<void> {
-  const pkgJson = await Bun.file(join(process.cwd(), "package.json")).json() as Json;
+  const {values} = parseArgs({
+    options: prepublishOptions,
+    allowPositionals: true,
+  });
+  const artifactsDir = values.artifacts as string | undefined;
+  if (artifactsDir == null) {
+    throw new Error("Missing artifacts parameter");
+  }
+  const bunCwd = process.cwd();
+  const rootPkgJsonPath = join(bunCwd, "package.json");
+  const pkgJson = await Bun.file(rootPkgJsonPath).json() as Json;
   const config = await getConfigFromPkgJson(pkgJson);
+  const zigCwd = join(bunCwd, config.zigCwd);
+  const artifactsDirPath = join(zigCwd, artifactsDir);
+  if (await Bun.file(artifactsDirPath).exists() == false) {
+    throw new Error(`Artifacts directory "${artifactsDir}" does not exist`);
+  }
+
   for (const target of config.targets) {
     await writeTargetPackage(pkgJson, config, target);
-    await build({
-      target,
-      optimize: config.optimize,
-      zigCwd: config.zigCwd,
-    });
-    await copyTargetLibrary(config, target);
+    await copyTargetLibrary(config, target, artifactsDirPath);
   }
   const updatedPkgJson = await updateOptionalDependencies(pkgJson, config);
   await Bun.write(
-    join(process.cwd(), "package.json"),
+    rootPkgJsonPath,
     JSON.stringify(updatedPkgJson, null, 2),
   );
 }
